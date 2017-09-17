0c060da408010a224b547626cfe8970ec5759b7e
Piotr Krajewski <pkrajewski@rtclab.com>
2017-09-17 15:32:52 +0200

    Support MorphOne and MorphMany

--- src/Traits/HasNestedAttributesTrait.php
+++ src/Traits/HasNestedAttributesTrait.php
@@ -5,7 +5,9 @@ namespace Eloquent\NestedAttributes\Traits;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Builder;
 
 trait HasNestedAttributesTrait
@@ -65,32 +67,20 @@ trait HasNestedAttributesTrait
 
         foreach ($this->getAcceptNestedAttributesFor() as $attribute => $stack) {
             $methodName = lcfirst(join(array_map('ucfirst', explode('_', $attribute))));
-        
+    
             if (!method_exists($this, $methodName)) {
                 throw new Exception('The nested atribute relation "' . $methodName . '" does not exists.');
             }
-        
+
             $relation = $this->$methodName();
             
-            if ($relation instanceof HasOne) {
-                if ($this->exists && $model = $relation->first()) {
-                    if (!$model->update($stack)) {
-                        return false;
-                    }
-                } else if (!$relation->create($stack)) {
+            if ($relation instanceof HasOne || $relation instanceof MorphOne) {
+                if (!$this->saveNestedAttributes($relation, $stack)) {
                     return false;
                 }
-            } else if ($relation instanceof HasMany) {
-                foreach ($stack as $attrs) {
-                    if (isset($attrs['id']) && $this->exists) {
-                        if (!$model = $relation->find($attrs['id'])) {
-                            throw new Exception('Not found.');
-                        }
-
-                        if (!$model->update($attrs)) {
-                            return false;
-                        }
-                    } else if (!$relation->create($attrs)) {
+            } else if ($relation instanceof HasMany || $relation instanceof MorphMany) {
+                foreach ($stack as $params) {
+                    if (!$this->saveNestedAttributes($relation, $params)) {
                         return false;
                     }
                 }
@@ -101,5 +91,27 @@ trait HasNestedAttributesTrait
 
         DB::commit();
         return true;
+    }
+
+    /**
+     * Save the nested relation attributes to the database.
+     *
+     * @param  Illuminate\Database\Eloquent\Relations  $relation
+     * @param  array                                   $params
+     * @return bool
+     */
+    protected function saveNestedAttributes($relation, array $params)
+    {
+        if ($this->exists) {
+            if (isset($params['id'])) {
+                $model = $relation->findOrFail($params['id']);
+            } else {
+                $model = $relation->firstOrFail();
+            }
+            return $model->update($params);
+        } else if ($relation->create($params)) {
+            return true;
+        }
+        return false;
     }
 }
--- tests/HasNestedAttributesTraitTest.php
+++ tests/HasNestedAttributesTraitTest.php
@@ -23,35 +23,60 @@ class HasNestedAttributesTraitTest extends TestCase
         Mockery::close();
     }
 
+    /**
+     * The fillable test.
+     */
     public function testFillable()
     {
-        $params = [
-            'title' => 'foo',
-
+        $payload = [
+            'title'      => 'foo',
             'not_exists' => [],
-
-            'model_bar' => [
-                'text' => 'bar'
-            ],
-
+            'model_bar'  => ['text' => 'bar'],
             'model_foos' => [
-                ['text' => 'foo']
+                ['text' => 'foo1'],
+                ['text' => 'foo2'],
             ]
         ];
 
-        $this->model->fill($params);
+        $this->model->fill($payload);
 
         $this->assertEquals(['title' => 'foo'], $this->model->getAttributes());
-
         $this->assertEquals([
             'model_bar'  => ['text' => 'bar'],
-            'model_foos' => [['text' => 'foo']]
+            'model_foos' => [
+                ['text' => 'foo1'], 
+                ['text' => 'foo2']
+            ]
         ], $this->model->getAcceptNestedAttributesFor());
     }
 }
 
 class ModelEloquentStub extends Model {
-    protected $table = 'stub';
+    protected $table    = 'stubs';
     protected $fillable = ['title'];
-    protected $nested = ['model_bar', 'model_foos' ];
+    protected $nested   = ['model_bar', 'model_foos' ];
+    
+    public function modelBar() {
+        return $this->hasOne(ModelBarStub::class);
+    }
+
+    public function modelFoos() {
+        return $this->hasOne(ModelFooStub::class);
+    }
+}
+
+class ModelBarStub extends Model {
+    protected $fillable = ['text'];
+    
+    public function parent() {
+        return $this->belongsTo(ModelEloquentStub::class);
+    }
+}
+
+class ModelFooStub extends Model {
+    protected $fillable = ['text'];
+
+    public function parent() {
+        return $this->belongsTo(ModelEloquentStub::class);
+    }
 }
